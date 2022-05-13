14ac0bab79ff5a9ac40af65ccf380df7d56e1a74
AdrianoS96 <adrianodes96@gmail.com>
2022-05-12 21:01:01 -0300

    Initial commit

---
+++ reconhecimento_fala.py
@@ -0,0 +1,22 @@
+#pip install SpeechRecognition
+#pyaudio, digite no google "install pyaudio seu_sistema_operacional" - 
+# pip install pipwin
+# pipwin install pyaudio - pip install pyaudio
+from cgitb import text
+import speech_recognition as sr
+
+
+rec = sr.Recognizer()
+
+#print(sr.Microphone().list_microphone_names()) # printa a lista de microfones existentes no computador
+# sr.Microphone() - recebe como parametro o indice do microfone que será utilizado
+with sr.Microphone(1) as mic:
+    #ajusta o microfone para não captar som ambiente
+    rec.adjust_for_ambient_noise(mic)
+    print('Pode falar que eu vou gravar')
+    #grava o audio
+    audio = rec.listen(mic)
+    # faz o reconhecimento do audio e passa para texto
+    #           biblioteca do google
+    texto = rec.recognize_google(audio, language="pt-BR")
+    print(texto)
